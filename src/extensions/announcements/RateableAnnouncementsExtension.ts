24c774d00dab85282b4336a922c8bde27deb76fe
denniskuhn <dennis@kuhn.geek.nz>
2021-10-02 19:28:29 +1300

    Increment version in solution

--- src/extensions/announcements/RateableAnnouncementsExtension.ts
+++ src/extensions/announcements/RateableAnnouncementsExtension.ts
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Log } from '@microsoft/sp-core-library';
 import { BaseApplicationCustomizer, PlaceholderName } from '@microsoft/sp-application-base';
 import * as Controller from '@mauriora/controller-sharepoint-list';
-
 import { IAnnouncementsProps, Announcements } from './components/Announcements';
 import { configure } from 'mobx';
 
@@ -18,45 +16,42 @@ export interface IRateableAnnouncementsExtensionProperties {
 /**
  * Mobx Configuration
  */
- configure({
+configure({
     enforceActions: "never"
-  });  
+});
 
-export default class RateableAnnouncementsExtension
-    extends BaseApplicationCustomizer<IRateableAnnouncementsExtensionProperties> {
+export default class RateableAnnouncementsExtension extends BaseApplicationCustomizer<IRateableAnnouncementsExtensionProperties> {
 
     protected async onInit(): Promise<void> {
-        console.log(`${this.context.manifest.alias} [${this.context.manifest.id}] version=${this.context.manifest.version} onInit super.onInit...`, { context: this.context, properties: this.properties });
         super.onInit();
-        await Controller.init(this.context);
 
         if (!this.properties.siteUrl || !this.properties.listName || !this.properties.acknowledgedListName) {
-            console.error(`${this.context.manifest.alias} [${this.context.manifest.id}] version=${this.context.manifest.version} onInit Missing required configuration parameters`, { context: this.context, properties: this.properties });
-            const e: Error = new Error('Missing required configuration parameters');
-            Log.error(QUALIFIED_NAME, e);
-            return Promise.reject(e);
+            const message = `${this.context.manifest.alias} [${this.context.manifest.id}] version=${this.context.manifest.version} onInit Missing required configuration parameters`;
+            console.error(message, { context: this.context, properties: this.properties });
+            return Promise.reject(new Error(message));
         }
+        await Controller.init(this.context);
+
         const header = this.context.placeholderProvider.tryCreateContent(PlaceholderName.Top);
 
         if (!header) {
-            console.error(`${this.context.manifest.alias} [${this.context.manifest.id}] version=${this.context.manifest.version} onInit Could not find placeholder Top`, { context: this.context, properties: this.properties });
-            const error = new Error('Could not find placeholder Top');
-            Log.error(QUALIFIED_NAME, error);
-            return Promise.reject(error);
+            const message = `${this.context.manifest.alias} [${this.context.manifest.id}] version=${this.context.manifest.version} onInit Could not find placeholder Top`;
+            console.error(message, { context: this.context, properties: this.properties });
+            return Promise.reject(new Error(message));
         }
 
         const site = this.context.pageContext.site;
         const tenantUrl = site.absoluteUrl.replace(site.serverRelativeUrl, "");
 
-        const elem: React.ReactElement<IAnnouncementsProps> = React.createElement(Announcements, { 
-            siteUrl: `${tenantUrl}${this.properties.siteUrl}`, 
+        const elem: React.ReactElement<IAnnouncementsProps> = React.createElement(Announcements, {
+            siteUrl: `${tenantUrl}${this.properties.siteUrl}`,
             listName: this.properties.listName,
             acknowledgedListName: this.properties.acknowledgedListName,
             culture: this.context.pageContext.cultureInfo.currentUICultureName
-         });
+        });
 
         ReactDOM.render(elem, header.domElement);
 
-        console.log(`${this.context.manifest.alias} [${this.context.manifest.id}] version=${this.context.manifest.version} onInit finished`, {propertiesDeconstructed: {...this.properties}, properties: this.properties, context: this.context, contextDeconstructed: {...this.context}});
+        console.log(`${this.context.manifest.alias} [${this.context.manifest.id}] version=${this.context.manifest.version} onInit finished`, { propertiesDeconstructed: { ...this.properties }, properties: this.properties, context: this.context, contextDeconstructed: { ...this.context } });
     }
 }
