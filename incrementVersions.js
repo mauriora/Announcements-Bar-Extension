1d34f33df4e58da4387e28df2866dc4fd14ac286
denniskuhn <dennis@kuhn.geek.nz>
2021-10-11 17:30:00 +1300

    Use chalk

--- incrementVersions.js
+++ incrementVersions.js
@@ -3,21 +3,19 @@ const packagePath = './package.json';
 const solutionPath = './config/package-solution.json';
 
 const versiony = require('versiony')
-
+const chalk = require('chalk');
 
 const updateFile = (file, version, nestedObject, arrayProperty) => {
     const newVersion = `${version}.0`;
-    const targetName = `${file}${nestedObject ? '.' + nestedObject : '' }`;
-    console.log(`Updating ${targetName}.version=${newVersion}`);
+    const targetName = `${chalk.cyan( file )}${nestedObject ? '.' + chalk.green( nestedObject ) : '' }`;
+    console.log(`Updating ${targetName}.version=${chalk.yellow(newVersion)}`);
     const fileContent = require(file);
     const target = nestedObject ? fileContent[nestedObject] : fileContent;
 
     target.version = newVersion;
     if( typeof target[arrayProperty] === 'object' ) {
-        console.log(`Updating ${target[arrayProperty].length} in ${targetName}.${arrayProperty} `);
-
         for( const item of target[arrayProperty]) {
-            console.log(`Updating ${targetName}.${arrayProperty}[${item['title'] ?? item['id']}]`);
+            console.log(`Updating ${targetName}.${chalk.blueBright( arrayProperty )}[${chalk.green( item['title'] ?? item['id'])}].version=${chalk.yellow(newVersion)}`);
             item['version'] = newVersion;
         }
     } else {
@@ -33,7 +31,7 @@ function main() {
         .with(packagePath)
         .end({quiet: true});
     
-    console.log(`Updated ${packagePath} to ${newVersion.version}`);
+    console.log(`Updated ${chalk.cyan( packagePath )} to ${chalk.yellow( newVersion.version )}`);
     updateFile( solutionPath, newVersion.version, 'solution', 'features' );
 }
 
